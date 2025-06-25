aac4b5f4f6ae1d43cff6d1338c3572a3149694ee
imanomura <206794822+imanomura@users.noreply.github.com>
2025-06-25 14:08:42 +0900

    通知

--- schedule.js
+++ schedule.js
@@ -6,3 +6,13 @@ window.onload = function () {
   Notification.requestPermission();
   const notification = new Notification('Check!');
 };
+
+const checktime = function () {
+  const currentTime = new Date();
+  console.log('currentTime');
+  const minutes = currentTime.getMinutes();
+  if (previousMinutes !== minutes && minutes % 15 === 0) {
+    previousMinutes = minutes;
+    const notification = new Notification('Check!');
+  }
+};
