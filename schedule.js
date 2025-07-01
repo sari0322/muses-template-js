88aa805fd559893f4db3280f7d7e35b5fe43f8ba
imanomura <206794822+imanomura@users.noreply.github.com>
2025-07-01 13:11:27 +0900

    確認

--- schedule.js
+++ schedule.js
@@ -7,9 +7,21 @@ window.onload = function () {
   const notification = new Notification('Check!');
 };
 
+//通知
+const start = document.getElementById('start');
+const end = document.getElementById('end');
+const notification = document.getElementById('notification');
+
 const checktime = function () {
-  const currentTime = new Date();
-  console.log('currentTime');
+  const currentTime = new Date(); // 現在時刻
+
+  if (start.value) {
+    const starttime = new Date(start.value);
+    if (Notification.permission === 'granted') {
+      new Notification('開始時間になりました！（毎日通知）');
+    }
+  }
+
   const minutes = currentTime.getMinutes();
   if (previousMinutes !== minutes && minutes % 15 === 0) {
     previousMinutes = minutes;
